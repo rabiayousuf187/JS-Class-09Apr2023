da3ab72d0339f7075d13f3b60335088ef0e6b528
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-04-09 10:39:10 +0500

    Chapter: 29     Converting strings to numbers, numbers to String

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('JS  Dated: 09 Apr 2023');
-// Chapter: 28  Converting strings to integers and decimals
+// // Chapter: 28  Converting strings to integers and decimals
+
+console.log("Chapter: 28  Converting strings to integers and decimals");
 
 var age = prompt("Enter Your Age:"); //22.5
 var new_age;
@@ -12,3 +14,23 @@ console.log("Convert String to Float USing parseFloat() Using  ==== ",new_age = 
 console.log("Convert String to Int Using parseInt() ==== ", parseInt(age), typeof(parseInt(age)));
 
 console.log("Convert String to Int OR Float USing Number()  ==== ", Number(age), typeof(Number(age)));
+
+// Chapter: 29     Converting strings to numbers, numbers to strings
+
+console.log("Chapter: 29     Converting strings to numbers, numbers to String");
+
+let book_ID = "125351";
+let float_Num = "25.565";
+let num = "1096" ,Convert;
+
+
+// Converting strings to numbers
+console.log("book_ID ==== ",book_ID, typeof(book_ID));
+
+console.log("Convert book_ID String to Number Using Number() ==== ", Convert = Number(book_ID), typeof(Convert));
+
+console.log("Convert String to Number Using Number() ==== ", Convert = Number(float_Num), typeof(Convert));
+
+// Converting numbers to String
+console.log("Convert Number to String USing  ==== ", Convert = num.toString(), typeof(Convert));
+
